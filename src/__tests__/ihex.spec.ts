fa5892d2605b54685e2621bed108ebc373b59319
microbit-carlos <carlos@microbit.org>
2020-04-22 17:38:25 +0100

    ihex: Add functions for detect valid records, the record type, and more.
    Also added functions to create an Extended Linear Address record.

--- src/__tests__/ihex.spec.ts
+++ src/__tests__/ihex.spec.ts
@@ -1,12 +1,12 @@
-import { createRecord, RecordType, endOfFileRecord } from '../ihex';
+import * as ihex from '../ihex';
 
-describe('Test createRecord()', () => {
+describe('Test createRecord() for standard records', () => {
   it('Create standard data records', () => {
     // Examples taken from a random micro:bit hex file
     expect(
-      createRecord(
+      ihex.createRecord(
         0x4290,
-        RecordType.Data,
+        ihex.RecordType.Data,
         new Uint8Array([
           0x64,
           0x27,
@@ -29,37 +29,104 @@ describe('Test createRecord()', () => {
     ).toEqual(':1042900064270020034B1960436803499B005A5070');
 
     expect(
-      createRecord(
+      ihex.createRecord(
         0xf870,
-        RecordType.Data,
+        ihex.RecordType.Data,
         new Uint8Array([0x00, 0x00, 0x00, 0x00])
       )
     ).toEqual(':04F870000000000094');
 
     expect(
-      createRecord(
+      ihex.createRecord(
         0xe7d4,
-        RecordType.Data,
+        ihex.RecordType.Data,
         new Uint8Array([0x0c, 0x1a, 0xff, 0x7f, 0x01, 0x00, 0x00, 0x00])
       )
     ).toEqual(':08E7D4000C1AFF7F0100000098');
   });
 
-  it('Create a custom BlockStart record', () => {
-    const result = createRecord(
-      0,
-      RecordType.BlockStart,
-      new Uint8Array([0x99, 0x01, 0xc0, 0xde])
-    );
-
-    expect(result).toEqual(':0400000A9901C0DEBA');
+  it('Create a custom End Of File record', () => {
+    expect(
+      ihex.createRecord(0, ihex.RecordType.EndOfFile, new Uint8Array([]))
+    ).toEqual(':00000001FF');
   });
+
+  // TODO: Add tests for the ExtendedSegmentAddress record
+  // TODO: Add tests for the StartSegmentAddress record
+  // TODO: Add tests for the ExtendedLinearAddress record
+  // TODO: Add tests for the StartLinearAddress record
+  // TODO: Add tests for all the thrown exceptions
+});
+
+describe('Test createRecord() for custom records', () => {
+  it('Create a custom BlockStart record', () => {
+    expect(
+      ihex.createRecord(
+        0,
+        ihex.RecordType.BlockStart,
+        new Uint8Array([0x99, 0x01, 0xc0, 0xde])
+      )
+    ).toEqual(':0400000A9901C0DEBA');
+  });
+
+  // TODO: Add tests for the BlockEnd record
+  // TODO: Add tests for the PaddedData record
+  // TODO: Add tests for the CustomData record
+  // TODO: Add tests for the OtherData record
+});
+
+describe('Test getRecordType() for standard records', () => {
+  it('Detect EoF record', () => {
+    expect(ihex.getRecordType(':00000001FF')).toEqual(
+      ihex.RecordType.EndOfFile
+    );
+    expect(ihex.getRecordType(':00000001FF\n')).toEqual(
+      ihex.RecordType.EndOfFile
+    );
+    expect(ihex.getRecordType(':00000001FF\r\n')).toEqual(
+      ihex.RecordType.EndOfFile
+    );
+    expect(ihex.getRecordType(':00000001FF\n\r')).toEqual(
+      ihex.RecordType.EndOfFile
+    );
+  });
+
+  // TODO: Add tests for the Data record
+  // TODO: Add tests for the ExtendedSegmentAddress record
+  // TODO: Add tests for the StartSegmentAddress record
+  // TODO: Add tests for the ExtendedLinearAddress record
+  // TODO: Add tests for the StartLinearAddress record
+  // TODO: Add tests for all the thrown exceptions
+});
+
+describe('Test getRecordType() for custom records', () => {
+  it('Detect Block Start record', () => {
+    expect(ihex.getRecordType(':0400000A9901C0DEBA')).toEqual(
+      ihex.RecordType.BlockStart
+    );
+  });
+
+  // TODO: Add tests for the BlockEnd record
+  // TODO: Add tests for the PaddedData record
+  // TODO: Add tests for the CustomData record
+  // TODO: Add tests for the OtherData record
 });
 
 describe('Test endOfFileRecord()', () => {
   it('Create a standard EoF record', () => {
-    const result = endOfFileRecord();
-
-    expect(result).toEqual(':00000001FF\n');
+    expect(ihex.endOfFileRecord()).toEqual(':00000001FF');
   });
+});
+
+describe('Test extLinAddressRecord()', () => {
+  it('Create Extended Linear Address records', () => {
+    expect(ihex.extLinAddressRecord(0x00000)).toEqual(':020000040000FA');
+    expect(ihex.extLinAddressRecord(0x04321)).toEqual(':020000040000FA');
+    expect(ihex.extLinAddressRecord(0x10000)).toEqual(':020000040001F9');
+    expect(ihex.extLinAddressRecord(0x20000)).toEqual(':020000040002F8');
+    expect(ihex.extLinAddressRecord(0x30000)).toEqual(':020000040003F7');
+    expect(ihex.extLinAddressRecord(0x31234)).toEqual(':020000040003F7');
+  });
+
+  // TODO: Add tests for all thrown exceptions
 });
--- src/ihex.ts
+++ src/ihex.ts
@@ -1,6 +1,6 @@
-import { byteArrayToHexStr, concatUint8Arrays } from './utils';
+import { byteArrayToHexStr, byteToHexStr, concatUint8Arrays } from './utils';
 
-const enum RecordType {
+enum RecordType {
   Data = 0x00,
   EndOfFile = 0x01,
   ExtendedSegmentAddress = 0x02,
@@ -14,9 +14,46 @@ const enum RecordType {
   OtherData = 0x0e,
 }
 
-const RECORD_LENGTH = 16;
+/**
+ * The maximum data bytes per record is 0xFF, 16 and 32 bytes are the two most
+ * common lengths, but to start we'll only support 16 bytes
+ */
+const RECORD_DATA_MAX_LENGTH = 16;
 
-function calcChecksum(dataBytes: Uint8Array): number {
+/**
+ * Constants for the record character lengths.
+ */
+const START_CODE_STR = ':';
+const START_CODE_STR_LEN = START_CODE_STR.length;
+const BYTE_COUNT_STR_LEN = 2;
+const ADDRESS_STR_LEN = 4;
+const RECORD_TYPE_STR_LEN = 2;
+const DATA_STR_LEN_MIN = 0;
+const CHECKSUM_STR_LEN = 2;
+const MIN_RECORD_STR_LEN =
+  START_CODE_STR_LEN +
+  BYTE_COUNT_STR_LEN +
+  ADDRESS_STR_LEN +
+  RECORD_TYPE_STR_LEN +
+  DATA_STR_LEN_MIN +
+  CHECKSUM_STR_LEN;
+const MAX_RECORD_STR_LEN =
+  START_CODE_STR_LEN +
+  BYTE_COUNT_STR_LEN +
+  ADDRESS_STR_LEN +
+  RECORD_TYPE_STR_LEN +
+  RECORD_DATA_MAX_LENGTH +
+  CHECKSUM_STR_LEN;
+
+/**
+ * Calculates the Intel Hex checksum.
+ *
+ * This is basically the LSB of the two's complement of the sum of all bytes.
+ *
+ * @param dataBytes A byte array to calculate the checksum into.
+ * @returns Checksum byte.
+ */
+function calcChecksumByte(dataBytes: Uint8Array): number {
   const sum = dataBytes.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     0
@@ -24,6 +61,15 @@ function calcChecksum(dataBytes: Uint8Array): number {
   return -sum & 0xff;
 }
 
+/**
+ * Creates an Intel Hex record with normal or custom record types.
+ *
+ * @param address - The two least significant bytes for the data address.
+ * @param recordType - Record type, could be one of the standard types or any
+ *    of the custom types created for forming fat binaries.
+ * @param dataBytes - Byte array with the data to include in the record.
+ * @returns A string with the Intel Hex record.
+ */
 function createRecord(
   address: number,
   recordType: RecordType,
@@ -33,28 +79,102 @@ function createRecord(
     throw new Error('Custom record address out of range.');
   }
   const byteCount = dataBytes.length;
-  if (byteCount > RECORD_LENGTH) {
-    throw new Error('Custom record data has to many bytes.');
+  if (byteCount > RECORD_DATA_MAX_LENGTH) {
+    throw new Error('Custom record data has too many bytes.');
   }
 
-  const recordData = concatUint8Arrays([
+  const recordContent = concatUint8Arrays([
     new Uint8Array([byteCount]),
     new Uint8Array([address >> 8, address & 0xff]),
     new Uint8Array([recordType]),
     dataBytes,
   ]);
-  const recordDataStr = byteArrayToHexStr(recordData);
-  const checksumStr = calcChecksum(recordData).toString(16).padStart(2, '0');
-  return `:${recordDataStr}${checksumStr}`.toUpperCase();
+  const recordContentStr = byteArrayToHexStr(recordContent);
+  const checksumStr = byteToHexStr(calcChecksumByte(recordContent));
+  return `${START_CODE_STR}${recordContentStr}${checksumStr}`.toUpperCase();
 }
 
 /**
- * No need to use createRecord() in this case as this record is always the same.
+ * Check if the an Intel Hex record conforms to the following rules:
+ *  - Correct length of characters
+ *  - Starts with a colon
+ *
+ * TODO: Apply more rules.
+ *
+ * @param iHexRecord - Single Intel Hex record to check.
+ * @returns A boolean indicating if the record is valid.
+ */
+function isValidRecord(iHexRecord: string): boolean {
+  if (
+    iHexRecord.length < MIN_RECORD_STR_LEN ||
+    iHexRecord.length > MAX_RECORD_STR_LEN ||
+    iHexRecord[0] !== ':'
+  ) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Retrieves the Record Type form an Intel Hex record line.
+ *
+ * @param iHexRecord A single Intel Hex record.
+ * @returns The RecordType value.
+ */
+function getRecordType(iHexRecord: string): RecordType {
+  // TODO: This will trim white spaces and new lines anywhere, we need to
+  // replace it with something that just removes the \r and \n from the end of
+  // the line
+  iHexRecord = iHexRecord.trim();
+  if (!isValidRecord(iHexRecord)) {
+    throw new Error('Record is not valid.');
+  }
+  const recordTypeCharStart =
+    START_CODE_STR_LEN + BYTE_COUNT_STR_LEN + ADDRESS_STR_LEN;
+  const recordTypeStr = iHexRecord.slice(
+    recordTypeCharStart,
+    recordTypeCharStart + RECORD_TYPE_STR_LEN
+  );
+  const recordType = parseInt(recordTypeStr, 16);
+  if (!(recordType in RecordType)) {
+    throw new Error(`Record type '${recordTypeStr}' is not valid.`);
+  }
+  return recordType;
+}
+
+/**
+ * Creates an End Of File Intel Hex record.
  *
  * @returns End of File record with new line.
  */
 function endOfFileRecord(): string {
-  return ':00000001FF\n';
+  // No need to use createRecord(), this record is always the same
+  return ':00000001FF';
 }
 
-export { RecordType, createRecord, endOfFileRecord };
+/**
+ * Creates an Extended Linear Address record from a 4 byte address.
+ *
+ * @param address - Full 32 bit address.
+ * @returns The Extended Linear Address Intel Hex record.
+ */
+function extLinAddressRecord(address: number): string {
+  if (address < 0 || address > 0xffffffff) {
+    throw new Error(
+      `Address '${address}' for Extended Linear Address record is in range.`
+    );
+  }
+  return createRecord(
+    0,
+    RecordType.ExtendedLinearAddress,
+    new Uint8Array([(address >> 24) & 0xff, (address >> 16) & 0xff])
+  );
+}
+
+export {
+  RecordType,
+  createRecord,
+  getRecordType,
+  endOfFileRecord,
+  extLinAddressRecord,
+};
