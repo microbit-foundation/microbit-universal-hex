b868c014b4d681a235cfc96ade11818f176e81e9
microbit-carlos <carlos@microbit.org>
2020-04-29 12:39:22 +0100

    fat-binary: Initial version of iHexToCustomFormat().
    Creates a file that can currently be consumed by DAPLink, but does
    do block padding yet.

--- src/fat-binary.ts
+++ src/fat-binary.ts
@@ -1,16 +1,5 @@
 import * as ihex from './ihex';
 
-function createStartRecord(boardId: number): string {
-  if (boardId < 0 || boardId > 0xffff) {
-    throw new Error('Board ID out of range.');
-  }
-  return ihex.createRecord(
-    0,
-    ihex.RecordType.BlockStart,
-    new Uint8Array([boardId >> 8, boardId && 0xff, 0xc0, 0xde])
-  );
-}
-
 /**
  *
  * @throws {Error} When the Board ID is not between 0 and 2^16.
@@ -20,22 +9,49 @@ function createStartRecord(boardId: number): string {
  * @returns New Intel Hex string with the custom format.
  */
 function iHexToCustomFormat(iHexStr: string, boardId: number): string {
-  const startRecord = createStartRecord(boardId);
-  // const endRecord = createEndRecord();
-  const hexLines = iHexStr.split('\r\n');
+  const startRecord = ihex.blockStartRecord(boardId);
 
   const blockLines = [];
-  // First we need to check if the first line is an extended linear record
-  blockLines.push(
-    ihex.getRecordType(hexLines[0]) === ihex.RecordType.ExtendedLinearAddress
-      ? hexLines[0]
-      : ihex.extLinAddressRecord(0)
-  );
-  blockLines.push(startRecord);
+  const hexRecords = ihex.iHexToRecordStrs(iHexStr);
+  let currentExtAddr = ihex.extLinAddressRecord(0);
+  let ih = 0;
+  const hexLength = hexRecords.length;
+  while (ih < hexLength) {
+    // First we need to check if the first line is an extended linear record
+    const firstRecordType = ihex.getRecordType(hexRecords[ih]);
+    if (firstRecordType === ihex.RecordType.ExtendedLinearAddress) {
+      currentExtAddr = hexRecords[ih];
+      ih++;
+    }
+    blockLines.push(currentExtAddr);
+    blockLines.push(startRecord);
 
-  // for (let currentLine = 0; currentLine < hexLines.length; currentLine++) {}
+    const loopEnd = Math.min(10, hexLength - ih);
+    let endOfFile = false;
+    for (let j = 0; j < loopEnd; j++) {
+      let record = hexRecords[ih++];
+      const recordType = ihex.getRecordType(record);
+      if (recordType === ihex.RecordType.Data) {
+        record = ihex.convertRecordToCustomData(record);
+      } else if (recordType === ihex.RecordType.EndOfFile) {
+        endOfFile = true;
+        // Error if we encounter an EoF record and it's not the end of the file
+        if (ih !== hexLength) {
+          throw new Error(`EoF record found at line ${ih} of ${hexLength}`);
+        }
+        break;
+      } else if (ihex.RecordType.ExtendedLinearAddress) {
+        currentExtAddr = record;
+      }
+      blockLines.push(record);
+    }
+    blockLines.push(ihex.blockEndRecord(0));
+    if (endOfFile) {
+      blockLines.push(ihex.endOfFileRecord());
+    }
+  }
 
-  return '';
+  return blockLines.join('\n');
 }
 
 export { iHexToCustomFormat };
